4f3218193da74d86e62fb53ab699b533dac0d0fe
izaudon <kkissy.0112@gmail.com>
2023-02-04 13:04:38 +0000

    update

--- src/handler.js
+++ src/handler.js
@@ -1,22 +1,35 @@
+import { setTimeout } from "node:timers/promises"
+
 export const handler = async msg => {
   if(msg.author.bot) return
   if (/伴田|路子/.test(msg.content)) {
     msg.channel.send("本名で呼ぶのはやめくださいってば〜！");
   }
-  if (/みちこ|路子/.test(msg.content)) {
+  if (/みちこ|路子|コロちゃん/.test(msg.content)) {
     msg.channel.send("ロコはロコです！");
   }
-  if(!/ロコ|ろこ|ROCO|roco/.test(msg.content)) return
-
-  // msg.content
-  // msg.author.username
-  // `<@${msg.author.id}>`
-  //console.log("message received from:", msg.author.username)
-  //msg.channel.send(`hello! <@${msg.author.id}>`)
-
+  if (/ROCO|Roco|roco|🇷 ?(🇴|🅾️) ?🇨 ?(🇴|🅾️) ?/.test(msg.content)){
+    let react = ["🇷", "🅾️", "🇨", "🇴"]
+    for (let i = 0;i<4;i++){
+      msg.react(react[i])
+    }
+  }
+  if (/art|Art|ART|アート|芸術/.test(msg.content)){
+    msg.react("🎨")
+  }
+  if(!/ロコ|ろこ|ROCO|roco|Roco|🇷 ?(🇴|🅾️) ?🇨 ?(🇴|🅾️) ?/.test(msg.content)) return
 
   switch(true) {
-    case /かわい|可愛|カワイ|えらい|偉/.test(msg.content):
+    case /みくじ/.test(msg.content): {
+      let arr = ["> \[**大吉**\]\nエクセレントです！", "\> [**吉**\]\nグレイトです！", "> \[**中吉**\]\nベリーグッドです！", "> \[**小吉**\]\nグッドです！", "> \[**末吉**\]\nナイスです！", "> \[**凶**\]\nバッドですね……", "> \[**大凶**\]\nテリブルです……", "> \[**ロコ吉**\]\nファンタスティックです!", "> \[**チュパ吉**\]\nラッキーです……？", "> \[**崖吉**\]\nラッキーなんですかね……？", "> \[**百万吉**\]\nワンダフルです！", "> \[**令和**\]\n🤔"];
+      let weight = [5, 10, 7, 7, 5, 3, 2, 1, 3, 3, 4, 2];
+      fortune(arr, weight);
+      break;
+    }
+    case /オウム返し|真似/.test(msg.content):
+      parrot()
+      break
+    case /かわい|可愛|カワイ|えらい|偉|すご|凄|流石|さすが/.test(msg.content):
       msg.channel.send("えへへ。サンクスです〜")
       break
     case /サンクス|さんくす/.test(msg.content):
@@ -41,22 +54,60 @@ export const handler = async msg => {
       msg.channel.send("何かあったのか、じゃないで")
       break
     case /はと|ハト|鳩|ぽっぽ|っぽ|ポッポ|ッポ/.test(msg.content):
-      msg.channel.send("ぽっぽ~です!")
+      msg.channel.send("ぽっぽ〜です!")
       break
-    case /アホ|あほ|阿呆|馬鹿|バカ|ばか|うんち|うんこ|クソ|くそ|糞|ボケ|ぼけ|カス|はげ|ハゲ/.test(msg.content):
+    case /アホ|あほ|阿呆|馬鹿|バカ|ばか|うんち|うんこ|クソ|くそ|糞|ボケ|ぼけ|カス|はげ|ハゲ|排泄/.test(msg.content):
       msg.channel.send("そんなこと言うなんて、ひどいです……")
       break
     case /バイバイ|さよなら|さようなら|またね|じゃ.*ね/.test(msg.content):
       msg.channel.send("バイバイです!プロデューサー！")
       break
-    case /みくじ/.test(msg.content):
-    let arr = ["\[大吉\]エクセレントです！", "\[吉\]グレイトです！", "\[中吉\]ベリーグッドです！", "\[小吉\]グッドです！", "\[末吉\]ナイスです！", "\[凶\]バッドですね……", "\[大凶\]テリブルです……", "\[ロコ吉\]ファンタスティックです!"];
-    let weight = [5, 10, 7, 7, 5, 3, 2, 1];
-    fortune(arr, weight);
-    break;
-    default:
-      msg.channel.send("どうかしましたか？")
-      // defaul
+    case /やった(〜|ー|あ|ぁ)|わ(ー|〜|あ|ぁ)い/.test(msg.content):
+      msg.channel.send("コングラッチュレーションです！")
+      break
+    case /あけおめ|あけまして|明けまして/.test(msg.content):
+      msg.channel.send("ハッピーニューイヤーです！プロデューサー！")
+      break
+    case /ほめて|褒めて|できた|出来た/.test(msg.content): {
+      let arr = ["コングラッチュレーションです！", "マーベラスです！", "ファンタスティックです！", "アメイジングです！", "エクセレントです！"]
+      let weight = [1,1,1,1,1]
+      fortune(arr,weight)
+      break
+    }
+    case /存在意義|レゾンテートル/.test(msg.content): {
+      let res = ["ロコのレゾンデートルって", "レプレゼンテーションそのものなんです！", "ロコモーティブにグラデーションする", "パッションのマニフェスト、プレゼンスがクレッシェンドして", "パラダイムシフトしちゃえば", "エモーションをコミットできちゃうんですよ！", "それって……って、聞いてますか？プロデューサー！"]
+      for (let i = 0;i<7;i++){
+        msg.channel.sendTyping()
+        await setTimeout(1200)
+        msg.channel.send(res[i])
+      }
+      break
+    }
+    case /モチベ|(m|M)otivation|MOTIVATION/.test(msg.content): {
+      let mot = ["クリエイションのモチベーションは", "インディビジュアルのストリームなんです！", "インセンティブされたインスティンクトがリジェクトされたパトスは", "ロコにオーサムなディスカバリーをギブしてくれるんです！"]
+      for (let i = 0;i<4;i++){
+        msg.channel.sendTyping()
+        await setTimeout(1200)
+        msg.channel.send(mot[i])
+      }
+      break
+    }
+    default:{
+      let arr = ["どうかしましたか？", "はい！", "はい！ロコですよ！", "ロコです！", "なんですか〜？プロデューサー！"]
+      let weight = [1,1,1,1,1]
+      fortune(arr, weight)
+      break
+    }
+  }
+
+  function parrot(){
+    msg.channel.send("イミテーションですね！")
+    const filter = msg => !msg.author.bot
+    const collected = msg.channel.awaitMessages({filter, max: 1, time: 20 * 1000 })
+    .then(collected => {
+      if(!collected.size) return msg.channel.send("何か言ってくださいよ〜……")
+      msg.channel.send(collected.first().content)
+    })
   }
 
   function fortune(arr, weight){
@@ -67,7 +118,7 @@ export const handler = async msg => {
       let random = Math.floor(Math.random()*totalweight);
       for(var i = 0;i<weight.length;i++){
         if(random<weight[i]){
-          msg.reply(arr[i])
+          msg.channel.send(arr[i])
           return;
         }else{
           random -= weight[i];
--- src/vc.js
+++ src/vc.js
@@ -1,40 +1,22 @@
+import config from "../config.js"
+
 export const vcHandler = client => async (oldState, newState) => {
   if (newState.channel && !oldState.channel) {
-    const channel = newState.channel;
-    if (channel.guild.id) {
-      //
-      const kikisenId = kikisen[channel.guild.id]
+    const kikisenId = config.vcLog[newState.channel.guild.id]
+    if (kikisenId) {
       const kikisenChannel = await client.channels.fetch(kikisenId);
-      kikisenChannel.send(
-        (
-          "`" +
-          newState.member.displayName +
-          "`" +
-          "さんが" +
-          "**" +
-          channel.name +
-          "**`" +
-          "にコネクトしました！"
-        ).replace("@", "＠")
-      );
+      await kikisenChannel.send(
+        `\`${newState.member.displayName}\`さんが` +
+        `**${newState.channel.name}**にコネクトしました！`)
     }
   } else if (!newState.channel && oldState.channel) {
-    const channel = oldState.channel;
-    if (channel.guild.id) {
-      const kikisenId = kikisen[channel.guild.id]
+    const kikisenId = config.vcLog[oldState.channel.guild.id]
+    if (kikisenId) {
       const kikisenChannel = await client.channels.fetch(kikisenId);
-      kikisenChannel.send(
-        (
-          "`" +
-          newState.member.displayName +
-          "`" +
-          "さんが" +
-          "**" +
-          channel.name +
-          "**" +
-          "からディスコネクトしました"
-        ).replace("@", "＠")
-      );
+      await kikisenChannel.send(
+          `\`${oldState.member.displayName}\`さんが` +
+          `**${oldState.channel.name}**からディスコネクトしました`)
     }
   }
-};
+}
+
